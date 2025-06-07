90af6bb5430b80ebbeca99872b800dbf9f0a9827
chanhengseang <chanheng.seang@mbanq.com>
2025-06-07 16:38:53 -0700

    tmd

--- eladmin-system/src/main/resources/db/migration/V18__Add_User_Type_Column.sql
+++ eladmin-system/src/main/resources/db/migration/V18__Add_User_Type_Column.sql
@@ -1,1 +1,4 @@
 ALTER TABLE sys_user ADD COLUMN user_type VARCHAR(50) COMMENT 'User type (PLAYER, ORGANIZER, ADMIN)';
+
+DELETE from `tool_email_config`;
+INSERT INTO `tool_email_config` VALUES (1,'chanheng.sng3@gmail.com','smtp.gmail.com','03792ACAD81EDFC53D2F19C68B451FD659C12F575B1101BA','465','SRR');
