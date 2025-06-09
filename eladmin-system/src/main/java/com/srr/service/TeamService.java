18c2727c8d983181b8ccca7dcb32471eda40bd9d
chanhengseang <chanheng.seang@mbanq.com>
2025-06-08 23:26:40 -0700

    merge it

---
+++ eladmin-system/src/main/java/com/srr/service/TeamService.java
@@ -0,0 +1,43 @@
+/*
+ *  Copyright 2019-2025 Zheng Jie
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.srr.service;
+
+import com.srr.dto.TeamDto;
+
+import java.util.List;
+
+/**
+ * @author Chanheng
+ * @website https://eladmin.vip
+ * @description Service interface for Team
+ * @date 2025-05-30
+ **/
+public interface TeamService {
+
+    /**
+     * Find a team by ID
+     * @param id Team ID
+     * @return TeamDto
+     */
+    TeamDto findById(Long id);
+
+    /**
+     * Find all teams for a specific event
+     * @param eventId Event ID
+     * @return List of TeamDto
+     */
+    List<TeamDto> findByEventId(Long eventId);
+}
---
+++ eladmin-system/src/main/java/com/srr/service/impl/QuestionServiceImpl.java
@@ -0,0 +1,125 @@
+package com.srr.service.impl;
+
+import com.srr.domain.MatchFormat;
+import com.srr.domain.Question;
+import com.srr.domain.Sport;
+import com.srr.dto.QuestionDto;
+import com.srr.repository.QuestionRepository;
+import com.srr.repository.SportRepository;
+import com.srr.service.QuestionService;
+import lombok.RequiredArgsConstructor;
+import me.zhengjie.exception.BadRequestException;
+import me.zhengjie.exception.EntityNotFoundException;
+import me.zhengjie.utils.*;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+/**
+* @description Question service implementation
+* @author Chanheng
+* @date 2025-05-31
+**/
+@Service
+@RequiredArgsConstructor
+public class QuestionServiceImpl implements QuestionService {
+
+    private final QuestionRepository questionRepository;
+    private final SportRepository sportRepository;
+
+    @Override
+    public PageResult<QuestionDto> queryAll(QuestionDto criteria, Pageable pageable) {
+        Page<Question> page = questionRepository.findAll((root, query, cb) -> 
+                QueryHelp.getPredicate(root, criteria, cb), pageable);
+        return PageUtil.toPage(page.map(this::toDto));
+    }
+
+    @Override
+    public List<QuestionDto> getAllForSelfAssessment() {
+        Long sportId = getSportIdByName("badminton");
+        return questionRepository.findBySportIdAndFormatOrderByCategoryAndOrderIndex(sportId, MatchFormat.DOUBLES).stream()
+                .map(this::toDto)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<QuestionDto> getByCategory(String category) {
+        return questionRepository.findByCategoryOrderByOrderIndex(category).stream()
+                .map(this::toDto)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    @Transactional(rollbackFor = Exception.class)
+    public QuestionDto create(QuestionDto resources) {
+        Question question = new Question();
+        question.setText(resources.getText());
+        question.setCategory(resources.getCategory());
+        question.setOrderIndex(resources.getOrderIndex());
+        question.setMinValue(resources.getMinValue());
+        question.setMaxValue(resources.getMaxValue());
+        return toDto(questionRepository.save(question));
+    }
+
+    @Override
+    @Transactional
+    public ExecutionResult update(QuestionDto resources) {
+        Question question = questionRepository.findById(resources.getId())
+                .orElseThrow(() -> new EntityNotFoundException(Question.class, "id", resources.getId().toString()));
+        Question updated = new Question();
+        updated.setId(question.getId());
+        updated.setText(resources.getText());
+        updated.setCategory(resources.getCategory());
+        updated.setOrderIndex(resources.getOrderIndex());
+        updated.setMinValue(resources.getMinValue());
+        updated.setMaxValue(resources.getMaxValue());
+        updated.setCreateTime(question.getCreateTime());
+        questionRepository.save(updated);
+        return ExecutionResult.of(updated.getId());
+    }
+
+    @Override
+    @Transactional
+    public ExecutionResult delete(Long id) {
+        // Verify question exists
+        if (!questionRepository.existsById(id)) {
+            throw new EntityNotFoundException(Question.class, "id", id.toString());
+        }
+        // Delete question
+        questionRepository.deleteById(id);
+        return ExecutionResult.ofDeleted(id);
+    }
+
+    @Override
+    public QuestionDto findById(Long id) {
+        Optional<Question> question = questionRepository.findById(id);
+        ValidationUtil.isNull(question, "Question", "id", id);
+        return toDto(question.get());
+    }
+
+    private QuestionDto toDto(Question question) {
+        QuestionDto dto = new QuestionDto();
+        dto.setId(question.getId());
+        dto.setText(question.getText());
+        dto.setCategory(question.getCategory());
+        dto.setOrderIndex(question.getOrderIndex());
+        dto.setMinValue(question.getMinValue());
+        dto.setMaxValue(question.getMaxValue());
+        dto.setCreateTime(question.getCreateTime());
+        dto.setUpdateTime(question.getUpdateTime());
+        return dto;
+    }
+
+    private Long getSportIdByName(String sportName) {
+        return sportRepository.findAll().stream()
+            .filter(s -> s.getName().equalsIgnoreCase(sportName))
+            .findFirst()
+            .map(Sport::getId)
+            .orElseThrow(() -> new BadRequestException("Sport not found: " + sportName));
+    }
+}
